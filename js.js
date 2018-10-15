807b342c6f267fdd4bd40345bcf0c72f3eb655b8
andreea onofriesei <bubuux9@gmail.com>
2018-10-15 15:34:20 +0300

    .....

--- js.js
+++ js.js
@@ -8,24 +8,24 @@ function resolve() {
         var x2 = null;
 
         if (a === 0) {
-            document.getElementById('type').innerHTML('Ecuatie de gradul 1');
+            document.getElementById('type').HTML('Ecuatie de gradul 1');
         }
         delta = b * b - 4 * a * c;
 
         if (delta < 0) {
-            document.getElementById('type').innerHTML('Ecuatie nu are solutii reale');
+            document.getElementById('type').HTML('Ecuatie nu are solutii reale');
         }
         else {
             if (delta === 0) {
                 x1 = -(b / 2 * a);
                 x2 = x1;
-                document.getElementById('x1').innerHTML(x1);
-                document.getElementById('x2').innerHTML(x2);
+                document.getElementById('x1').html(x1);
+                document.getElementById('x2').HTML(x2);
             } else {
                 x1 = (-b + (Math.sqrt(delta))) / (2 * a);
                 x2 = (-b - (Math.sqrt(delta))) / (2 * a);
-                document.getElementById('x1').innerHTML(x1);
-                document.getElementById('x2').innerHTML(x2);
+                document.getElementById('x1').HTML(x1);
+                document.getElementById('x2').HTML(x2);
             }
         }
 
